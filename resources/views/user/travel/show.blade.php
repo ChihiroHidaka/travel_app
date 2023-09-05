f51ddefb172511fc1815d5e3542bfdd7c3ad6851
chihiro hidaka <chihi.stu270@gmail.com>
2023-09-05 15:08:29 +0000

    0905 行程表のcss修正

--- resources/views/user/travel/show.blade.php
+++ resources/views/user/travel/show.blade.php
@@ -10,7 +10,7 @@
 @section('content')
 <!--下記はデータを引っ張って表示させる-->
 
-<div>
+<div class ="travel">
      <h1>{{$travel->title}}</h1> 
      <ul>
          <!--<li></li>-->
@@ -25,63 +25,24 @@
 <!--下記に行程表を追加すること-->
 <div>
     <p>行程表</p>
-    　<table id="travelPlan">
-        <thead>
-            <tr>
-                <th>開始時間</th>
-                <th>終了時間</th>
-                <th>目的地もしくは移動手段</th>
-                <th>備考</th>
-            </tr>
-        </thead>
-        
-        <tbody>
-        @foreach($plans as $plan)
-        <p>{{$plan->plan_date}}</p>
-        
-          <tr class="PlanCreate">
-                <td id="fromtime" name="from_time" type="time" value="{{$plan->from_time }}">{{$plan->from_time }}</td>
-                <td id="endtime" name="end_time" type="time" value="{{$plan->end_time}}">{{$plan->end_time}}</td>
-                <td id="plan" name="[plan" type="text" value="{{$plan->plan}}">{{$plan->plan}}</td>
-                <td id="remarks" name="remarks" type="text" value="{{$plan->remarks}}">{{$plan->remarks}}</td>
-                <td><a href="/user/plan/{{$plan->id}}/edit">編集</a></td>
-                <td><a href="/user/plan/{{$plan->id}}/delete">削除</a></td>
-            </tr>
-
-        </tbody>
-        </table>
-        @endforeach
-        
+    
         <div>
-             @foreach($plans as $plan)
+            @foreach($plans as $plan)
              <ul class="Plancreate">
               <li>
-                <span id="fromtime" name="from_time" type="time" value="{{$plan->from_time }}">{{$plan->from_time }}</span>
+                <span class="time" id="fromtime" name="from_time" type="time" value="{{$plan->from_time }}">{{$plan->from_time }}</span>
                 <div class="sch_box">
                   <p id="plan" name="[plan" type="text" value="{{$plan->plan}}">{{$plan->plan}}</p>
                   <p id="remarks" name="remarks" type="text" value="{{$plan->remarks}}">
                   {{$plan->remarks}} </p>
-                  <br><a href="/user/plan/{{$plan->id}}/edit">編集</a>
-                  <br><a href="/user/plan/{{$plan->id}}/delete">削除</a>
+                  <a href="/user/plan/{{$plan->id}}/edit">編集</a>
+                  <a href="/user/plan/{{$plan->id}}/delete">削除</a>
                   <br>
                  
                </div>
               </li>
-
             </ul>
+            @endforeach
         </div>
-        
-       
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
 </div>
 @endsection
